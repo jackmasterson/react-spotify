fff8a788c41477fc6165fc98a39dbd96849df893
Jack Masterson <jmasterson@cafemedia.com>
2017-12-21 19:17:56 -0500

    create types

---
+++ src/types/types.js
@@ -0,0 +1,26 @@
+// sign in/out
+export const SIGN_IN_REQUEST = 'SIGN_IN_REQUEST';
+export const SIGN_IN_SUCCESS = 'SIGN_IN_SUCCESS';
+export const SIGN_IN_FAILURE = 'SIGN_IN_FAILURE';
+
+// new user
+export const NEW_USER_REQUEST = 'NEW_USER_REQUEST';
+
+// api request
+export const API_REQUEST = 'API_REQUEST';
+export const API_SUCCESS = 'API_SUCCESS';
+export const API_FAILURE = 'API_FAILURE';
+
+// api types
+export const TRACK_REQUEST = 'TRACK_REQUEST';
+export const ALBUM_REQUEST = 'ALBUM_REQUEST';
+export const ARTIST_REQUEST = 'ARTIST_REQUEST';
+
+// CRUD-related
+export const OPTION_SELECTED = 'OPTION_SELECTED';
+export const OPTION_DELETED = 'OPTION_DELETED';
+export const OPTION_RANK_CHANGED = 'OPTION_RANK_CHANGED';
+
+// common changes
+export const INPUT_UPDATE = 'INPUT_UPDATE';
+export const SELECTION_CHANGED = 'SELECTION_CHANGED';
