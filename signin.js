7ba45d52526cf2fe6743ed3324bfa13355dc25c4
JackMasterson <jackmasterson5@gmail.com>
2018-01-18 21:00:43 -0500

    Updates

---
+++ signin.js
@@ -0,0 +1,7 @@
+// let submit = document.querySelector('#submit');
+// submit.addEventListener('click', () => {
+//     let un = document.querySelector('#username').value;
+//     let pw = document.querySelector('#password').value;
+
+//     console.log('clicked!', un, pw);
+// })
